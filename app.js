bf129a7c31bb22f9ce50ce873428a3f8f4d461b7
nola360 <adaramolajr@gmail.com>
2020-03-30 22:29:25 -0400

    added icon delete function

--- app.js
+++ app.js
@@ -1,61 +1,61 @@
 const add_button = document.querySelector('.add_button');
-const input = document.querySelector('.input');
-const list = document.querySelector('ul');
+const list = document.getElementById('list');
 const delete_button = document.querySelector('.delete_button');
-const x_icon = document.querySelector('i.fas.fa-times');
-const line_through = document.querySelectorAll('li i.fas.fa-times');
+const x_icon = document.querySelectorAll('i.fas.fa-times');
+const line_through = document.querySelector('a');
+const checkbox = document.querySelectorAll('.checkbox');
 
 
 
-
-// line_through.forEach(function (item) {
-//   item.style.backgroundColor = 'yellow';
-//   item.parentElement.remove();
-// })
-
-// console.log(line_through);
-
-
-
-// const newline_through = Array.from(line_through);
-
-// console.log(line_through);
-// line_through.forEach(function (item) {
+// checkbox.forEach(function (item) {
+//   const lis = document.querySelectorAll('li');
 //   item.addEventListener('click', function () {
-//     console.log('test');
+//     lis.classList.toggle('active');
 //   })
 // })
 
-
-
-
-
-// for (let i = 0; i < line_through.length; i++) {
-//   line_through[i].nextElementSibling.addEventListener('click', function () {
-//     line_through[i].classList.toggle('active');
-//   })
-// }
-
+// Adds Li with child link and icon
 add_button.addEventListener('click', function () {
+  // Prevents Error of null
+  let input = document.getElementById('input')
   if (input.value === '') {
     return false;
   }
-  let item = document.createElement('li');
+  // Create Input Elements
+  let list = document.querySelector('#list');
+  let li = document.createElement('li');
+  li.className = 'list-item';
   let link = document.createElement('a')
-  let x_icon = document.createElement('i');
-  x_icon.className = 'fas fa-times';
+  link.className = 'link-item';
   link.setAttribute('href', '#');
   link.innerHTML = input.value;
-  item.appendChild(link);
-  item.appendChild(x_icon);
-  list.appendChild(item);
-  input.value = ''
-  console.log(item);
+  li.appendChild(link)
+  list.appendChild(li)
+  let x = document.createElement('i');
+  x.className = 'fas fa-times';
+  li.appendChild(x);
+  input.value = '';
+  console.log(li.children[0])
 })
 
+// Create Line Through Items
+// line_through.forEach(function (item) {
+//   item.classList.toggle('active');
+// })
+
+// line_through.addEventListener('click', function () {
+//   line_through.classList.toggle('active');
+// })
 
 
+// Removes Li from List
+list.addEventListener('click', function (e) {
+  if (e.target.parentElement.classList.contains('list-item')) {
+    e.target.parentElement.remove();
+  }
+})
 
+// Deletes each Li from list
 delete_button.addEventListener('click', function () {
   let li = document.querySelector('li');
   if (li === null) {
